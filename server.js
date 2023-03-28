5f5e42aa5781e614964c8cef853920648708c1ad
Keigo277 <thyeonex.8888@au.com>
2023-03-28 17:28:21 +0900

    first commit

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const{engine}=require('express-handlebars');
 
 const express = require("express");
+const fileUpload = require("express-fileupload");
 const app = express();
 
 const PORT = 5000;
 
+app.use(fileUpload());
 
 app.engine("handlebars",engine());
 app.set('view engine','handlebars');
@@ -16,6 +18,13 @@ app.set("views","./views");
 
 app.get("/",(req,res)=>{
     res.render("home");
+})
+
+app.post("/",(req,res)=>{
+    if(!req.files)
+      return res.status(400).send("何も画像ファイルは入っていません。");
+    else
+      console.log(req.files);
 })
 
 app.listen(PORT,()=>{
